3904cefe63cd2f35a390d9412bd66dcb9f214577
Red <ProjektRed@buaa.edu.cn>
2024-04-07 00:37:32 +0800

    t3 编译测试

--- T3/t3_cpp/load.js
+++ T3/t3_cpp/load.js
@@ -1,9 +1,8 @@
 import m from './T3.cjs'
 
-var _mancala_board, _malloc, HEAP32, getValue
+var _mancala_board, _mancala_operator, _malloc, HEAP32, getValue
 await m().then(m => {
-    _mancala_board = m._mancala_board,
-        _malloc = m._malloc, HEAP32 = m.HEAP32, getValue = m.getValue
+    _mancala_board = m._mancala_board, _malloc = m._malloc, HEAP32 = m.HEAP32, getValue = m.getValue, _mancala_operator = m._mancala_operator
 });
 
 export function mancalaBoard(k, a, n) {
@@ -15,4 +14,10 @@ export function mancalaBoard(k, a, n) {
         result[i] = getValue(offset + i * 4, 'i32');
     }
     return result;
+}
+
+export function mancalaOperator(k, a) {
+    const p = _malloc(a.length * 4);
+    HEAP32.set(new Int32Array(a), p >> 2);
+    return _mancala_operator(k, p);
 }
--- T3/test.js
+++ T3/test.js
@@ -1,19 +1,24 @@
 import assert from "assert";
 
 // Choose proper "import" depending on your PL.
-import { mancalaOperator as op1 } from "./t3-2-as/build/release.js";
+// import { mancalaOperator as op1 } from "./t3-2-as/build/release.js";
 // import { mancala_operator as op1 } from "./t3_2_rust/pkg/t3_2_rust.js"
 // [Write your own "import" for other PLs.]
+import { mancalaOperator as op1 } from "./t3_cpp/load.js";
 
 // Choose proper "import" depending on your PL.
-import { mancalaOperator as op2 } from "./t3-2-as-rival/build/release.js";
+// import { mancalaOperator as op2 } from "./t3-2-as-rival/build/release.js";
 // import { mancala_operator as op2 } from "./t3_2_rust_rival/pkg/t3_2_rust.js"
 // [Write your own "import" for other PLs.]
+import { mancala_operator as op2 } from "./t3_toby/package/kalah.js";
 
 // Choose proper "import" depending on your PL.
-import { mancalaBoard as board } from "./t3-1-as/build/release.js";
+// import { mancalaBoard as board } from "./t3-1-as/build/release.js";
 // import { mancala_board as board } from "./t3_1_rust/pkg/t3_1_rust.js"
 // [Write your own "import" for other PLs.]
+import { mancalaBoard as board } from "./t3_cpp/load.js";
+// import { mancala_board as board } from "./t3_toby/package/kalah.js";
+
 
 let operator, status, operation, operationSequence, boardReturn, isEnded;
 let op1Result = 0, op2Result = 0;
@@ -21,7 +26,7 @@ let op1Time = 0, op2Time = 0, timeStamp = 0;
 
 // Firstly, start from op1.
 operator = 1;
-status = [4,4,4,4,4,4,0,4,4,4,4,4,4,0];
+status = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0];
 operation = 0;
 operationSequence = [];
 isEnded = false;
@@ -40,12 +45,13 @@ do {
         operationSequence.push(operation);
         boardReturn = board(2, operationSequence, operationSequence.length);
     }
+    console.log(operationSequence)
     if (boardReturn[14] == 1) {
         operator = 1;
-        status = boardReturn.slice(0,14);
+        status = boardReturn.slice(0, 14);
     } else if (boardReturn[14] == 2) {
         operator = 2;
-        status = boardReturn.slice(0,14);
+        status = boardReturn.slice(0, 14);
     } else {
         isEnded = true;
         op1Result += boardReturn[14] - 200;
@@ -55,7 +61,7 @@ do {
 
 // Now change to start from op2.
 operator = 2;
-status = [4,4,4,4,4,4,0,4,4,4,4,4,4,0];
+status = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0];
 operation = 0;
 operationSequence = [];
 isEnded = false;
@@ -76,17 +82,17 @@ do {
     }
     if (boardReturn[14] == 1) {
         operator = 1;
-        status = boardReturn.slice(0,14);
+        status = boardReturn.slice(0, 14);
     } else if (boardReturn[14] == 2) {
         operator = 2;
-        status = boardReturn.slice(0,14);
+        status = boardReturn.slice(0, 14);
     } else {
         isEnded = true;
         op1Result += boardReturn[14] - 200;
         op2Result -= boardReturn[14] - 200;
     }
 } while (!isEnded);
- 
+
 op1Time = op1Time / 1000;
 op2Time = op2Time / 1000;
 
